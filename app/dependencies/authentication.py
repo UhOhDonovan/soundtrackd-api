8a104841f9c5a34233cde641ac9607a1ac306db7
Donovan Bale <donovan@balehaus.org>
2024-11-05 13:44:38 -0600

    jwt credential authentication

--- app/dependencies/authentication.py
+++ app/dependencies/authentication.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from ..db_tools.database import Session, engine
 from ..db_tools.models import User
@@ -32,11 +32,21 @@ def get_user(username):
             return user.username
 
 
-def fake_decode_token(token):
-    user = get_user(token)
-    return user
-
-
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username)
+    if user:
+        return user
+    raise credentials_exception
+
