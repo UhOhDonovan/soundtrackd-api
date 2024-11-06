fc91e77faf38415cf7fb59cc7727de341480ebee
Donovan Bale <donovan@balehaus.org>
2024-11-06 13:25:14 -0600

    load secrets from .env file

--- app/dependencies/authentication.py
+++ app/dependencies/authentication.py
@@ -5,26 +5,24 @@ from ..db_tools.database import SessionDep
 from ..db_tools.models import User
 import jwt
 from jwt.exceptions import InvalidTokenError
-from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
+from .constants import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)):
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + expires_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep):
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep
+):
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
@@ -41,4 +39,3 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], sessio
     if user:
         return user
     raise credentials_exception
-
